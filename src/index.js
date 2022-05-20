9f123ee598c1653964dd6e037b7c466207167cc9
AliRezeHeidari <h118ar@gmail.com>
2022-05-20 15:17:23 +0430

    finish useState -> start useEffect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Todo from "./Todos";
 
-function App() {
+function Car() {
+  const [car, setCar] = useState({
+    brand: "ford",
+    model: "pride",
+    year: "1964",
+    color: "red",
+  });
+  //   const [brand, setBrand] = useState("Ford");
+  //   const [model, setModel] = useState("Mustang");
+  //   const [year, setYear] = useState("1964");
+  //   const [color, setColor] = useState("red");
+
+  const updateCar = () => {
+    
+    // shorthand
+    // setCar({...car, color: "green"});
+
+    setCar(car =>{
+      console.log(car)
+      return{...car, color: "green"}
+    })
+  };
   return (
     <>
-      <h1>سلام دنیا</h1>
-      <Todo />
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+      <button onClick={updateCar}>میشه کلیک کنی لاشی</button>
     </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Car />);
